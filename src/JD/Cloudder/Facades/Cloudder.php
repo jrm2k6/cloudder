b84afe5cce8676abc2b0b0fab970cb1bb9932583
Roger Vilà <rogervila@me.com>
2016-08-22 20:30:46 +0200

    Formatted in PSR-2

--- src/JD/Cloudder/Facades/Cloudder.php
+++ src/JD/Cloudder/Facades/Cloudder.php
@@ -1,13 +1,19 @@
-<?php namespace JD\Cloudder\Facades;
+<?php 
+
+namespace JD\Cloudder\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Cloudder extends Facade {
+class Cloudder extends Facade
+{
 
     /**
      * Get the registered name of the component.
      *
      * @return string
      */
-    protected static function getFacadeAccessor() { return 'cloudder'; }
+    protected static function getFacadeAccessor()
+    {
+        return 'cloudder';
+    }
 }
